e61d3e33fd184c8d09444a100c42221d7ca994b0
Gustavo Araujo <121242417+gustavofda@users.noreply.github.com>
2025-02-04 21:17:39 -0300

    style: ajustando o tamanho

--- src/components/Noticia.jsx
+++ src/components/Noticia.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 function Noticia({ titulo, descricao, imagem, link }) {
   return (
-    <div className="bg-gray-800 text-white p-4 rounded-lg shadow-md mb-4">
-      <img src={imagem} alt={titulo} className="w-full h-64 object-cover rounded-t-lg" />
+    <div className="bg-gray-800 text-white p-4 rounded-lg shadow-md mb-4 w-90 h-auto">
+      <img src={imagem} alt={titulo} className="w-full h-68 object-cover rounded-t-lg" />
       <h3 className="text-xl font-bold mt-4">{titulo}</h3>
       <p className="text-gray-300 mt-2">{descricao}</p>
       <a href={link} target="_blank" rel="noopener noreferrer" className="text-red-500 mt-4 inline-block">
